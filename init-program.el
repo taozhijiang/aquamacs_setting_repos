fc361cfd03e4d9bad473ff7f2f0ea7f8ac4fe2a8
ztao <t@taozj.org>
2019-09-30 15:33:31 +0800

    add flycheck

--- init-program.el
+++ init-program.el
@@ -6,4 +6,20 @@
 (global-set-key (kbd "C-c w") 'whitespace-mode)
 
 
+(setq gdb-many-windows t        ; use gdb-many-windows by default
+      gdb-show-main t)          ; Non-nil means display source file containing the main routine at startup
+
+
+;; exec-path-from-shell.el
+(require 'exec-path-from-shell) ;; if not using the ELPA package
+(exec-path-from-shell-initialize)
+(when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize))
+
+;; flycheck
+(add-to-list 'load-path
+    (concat CustHomeDir "/tools/flycheck/"))
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
 (provide 'init-program)
